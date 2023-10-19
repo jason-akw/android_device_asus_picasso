09fad9fd0a2c757be269087bad3bf5e900966339
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    picasso: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- device.mk
+++ device.mk
@@ -371,6 +371,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
