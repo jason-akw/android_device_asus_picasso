dad13019caf09f67ee22872ba0dd1080d639e429
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    picasso: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- device.mk
+++ device.mk
@@ -250,7 +250,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_performance_lahaina.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_lahaina.xml \
     $(LOCAL_PATH)/media/media_codecs_performance_lahaina_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_lahaina_vendor.xml
 
-$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+$(call soong_config_set_bool,stagefright,target_disable_thumbnail_block_model,true)
 
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-service \
