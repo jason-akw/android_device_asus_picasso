2637713dafb243a5953eed1d2df17188fce127c8
EdwardWu <bluehome.wu@gmail.com>
2026-06-14 23:36:40 +0800

    picasso: nfc: Drop unsupported eSE service
    The lineage-20 boot log shows the NXP p73 secure element service crash looping:
    
      Cmdline: /vendor/bin/hw/android.hardware.secure_element@1.2-service.p73
    
      Abort message: 'Could not register service for Secure Element HAL Iface (-2147483648).'
    
      init: process with updatable components 'vendor.secure_element_hal_service_1_2' exited 4 times before boot completed
    
    picasso does not currently provide a working eSE instance. Stop inheriting the p73 secure element product makefile, remove the eSE ODM manifest and feature SKU hook, and stop declaring secure_element HAL instances that are not backed by a running service.
    
    Keep the NXP NFC product makefile and UICC secure element library package intact.
    
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- device.mk
+++ device.mk
@@ -242,18 +242,13 @@ PRODUCT_PACKAGES += \
 
 # NFC
 $(call inherit-product, vendor/nxp/nfc/nfc-vendor-product.mk)
-$(call inherit-product, vendor/nxp/secure_element/se-vendor-product.mk)
 
 TARGET_USES_NQ_NFC := true
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_eSE/android.hardware.se.omapi.ese.xml \
     frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
 
-PRODUCT_USES_ESE := false
-
 PRODUCT_PACKAGES += \
-    android.hardware.secure_element@1.0:64 \
     android.hardware.secure_element@1.2.vendor \
     vendor.nxp.nxpnfc@1.0.vendor
 
