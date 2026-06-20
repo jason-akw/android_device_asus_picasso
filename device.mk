22a5d47abe4cb49c236fa89f18c07f0a50ae3eeb
EdwardWu <bluehome.wu@gmail.com>
2026-06-20 23:32:11 +0800

    picasso: Add AIDL radio compatibility service
    Lineage 21 framework expects AIDL radio HAL instances, but the stock lahaina radio stack still exposes HIDL android.hardware.radio@1.5 instances.
    
    260619_logcat.log reports missing VINTF declarations for android.hardware.radio.data.IRadioData/slot1 and android.hardware.radio.messaging.IRadioMessaging/slot1, followed by modem stats reads failing with ERROR_PHONE_NOT_AVAILABLE.
    
    Build the common HIDL-to-AIDL radio compatibility service and declare the dual-SIM AIDL radio slot instances while keeping the original HIDL QCRIL manifest entries for the vendor blobs.
    
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- device.mk
+++ device.mk
@@ -379,6 +379,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
 PRODUCT_PACKAGES += \
+    android.hardware.radio-service.compat \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
