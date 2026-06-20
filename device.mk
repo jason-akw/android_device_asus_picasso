a6b4f0c510ec8e3ec92b727a785389fc37ff2faf
EdwardWu <bluehome.wu@gmail.com>
2026-06-21 00:37:48 +0800

    picasso: Drop mock power stats service
    sake lineage-21 does not ship a mock powerstats HAL. Avoid declaring a fake provider just to silence the framework probe for android.hardware.power.stats.IPowerStats/default.
    
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- device.mk
+++ device.mk
@@ -314,7 +314,6 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power.stats@1.0-service.mock \
     android.hardware.power-service-qti \
     android.hardware.power@1.2.vendor
 
