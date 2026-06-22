b0e3cd3f6f2fba545fbc08a34f15d3afedac1992
EdwardWu <bluehome.wu@gmail.com>
2026-06-22 20:11:56 +0800

    picasso: media: Override QTI Codec2 seccomp policy
    The stock QTI Codec2 service crashes when scrcpy uses the hardware AVC encoder after the kernel tag updates.
    
    Logcat shows vendor-qti-media-c2-hal-1-0 blocked by minijail on mremap, followed by c2.qti.avc.encoder dying and MediaCodec cancelling the pending output dequeue request.
    
    Keep the Codec2 binary blobs stock, but move the editable seccomp policy into the device tree so we can allow the MREMAP_MAYMOVE form used by the newer stack.
    
    This is paired with the vendor tree cleanup that drops the same policy from proprietary blobs.
    
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- device.mk
+++ device.mk
@@ -240,7 +240,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml \
+    device/asus/picasso/configs/seccomp_policy/codec2.vendor.base-arm.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.base-arm.policy
 
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
