2ef17f9aee7b5a5a669af47a61ca888da0d44133
EdwardWu <bluehome.wu@gmail.com>
2026-06-23 20:08:37 +0800

    picasso: proprietary-files: Add libpowercore
    libvideooptfeature links against libpowercore. Add the missing 32-bit entry and ensure generated vendor modules can see the source-built libqti-perfd-client namespace.
    
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -19,6 +19,7 @@ from extract_utils.main import (
 
 namespace_imports = [
     'hardware/qcom-caf/sm8350',
+    'hardware/qcom-caf/common/libqti-perfd-client',
     'hardware/qcom-caf/wlan',
     'vendor/qcom/opensource/dataservices',
     'vendor/qcom/opensource/display',
