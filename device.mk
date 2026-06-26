7faf2752a069d085e9cd8b17be38a419af40411c
EdwardWu <bluehome.wu@gmail.com>
2026-06-26 20:47:50 +0800

    picasso: Fix boot HAL service declarations
    260626_logcat-3.log shows system_server being killed by Watchdog after the lockscreen is dismissed:
    
      W Watchdog: *** WATCHDOG KILLING SYSTEM PROCESS: Blocked in handler on display thread (android.display)
    
      at lineageos.hardware.LineageHardwareManager.getAIDLService
    
      at org.lineageos.platform.internal.display.OutdoorModeController.<init>
    
      at org.lineageos.platform.internal.display.LiveDisplayService.onBootPhase
    
    The declared sysfs AIDL SunlightEnhancement service aborts before it can register:
    
      F SunlightEnhancementService: None of SunlightEnhancement paths could be opened
    
    picasso already has a device-specific HIDL SunlightEnhancement service using /proc/globalHbm, and the same log shows it registers successfully. Stop installing the generic sysfs AIDL service and its SE VINTF fragment.
    
    The same log also shows several packaged HALs failing to register because their VINTF declarations are missing or too old. Add the standard fingerprint HIDL instance and align the btconfigstore, qtiradio, secure element, and tetheroffload control declarations with the versions actually used by the vendor services.
    
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- device.mk
+++ device.mk
@@ -168,7 +168,6 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.14.vendor \
     vendor.display.config@1.15.vendor \
     vendor.lineage.livedisplay-service.sdm \
-    vendor.lineage.livedisplay-service.sysfs \
     vendor.lineage.livedisplay@2.0-service.picasso \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
@@ -177,7 +176,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
 $(call soong_config_set,livedisplay_sdm,enable_dm,false)
-$(call soong_config_set,livedisplay_sysfs,enable_se,true)
 
 PRODUCT_COPY_FILES += \
     vendor/asus/picasso/proprietary/vendor/firmware/iris6_ccf1.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/iris6_ccf1b.fw \
